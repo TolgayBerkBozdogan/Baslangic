4bab7b7307058ed5f2b6abc17b3e893b2ff26de4
Tolgay Berk <ttb.explorer@gmail.com>
2023-08-23 16:44:33 +0300

    Baslangic

---
+++ Baslangic.java
@@ -0,0 +1,89 @@
+public class Baslangic {
+    public static void main(String[] args) {
+
+        /* int kisaKenar = 5, uzunKenar= 18;
+        int alan = kisaKenar * uzunKenar;
+        int cevre = 2*(kisaKenar+uzunKenar);
+        System.out.println("Diktörgenin Alanı =" + alan);
+        System.out.print("Diktörgenin Çevresi =" + cevre); */
+
+       /* float vFloat = 3.14F;
+        System.out.println(vFloat);
+        double vDouble =3.14;
+        System.out.println(vDouble); */
+
+        /* char vChar = 'b';
+        char v2Char = 98;
+        System.out.println(vChar);
+        System.out.println(v2Char);
+        char c1 = 'J';
+        char c2 = 'A';
+        char c3 = 'V';
+        char c4 = 'A';
+        System.out.println(c1+ c2+ c3+ c4);
+        System.out.println("" + c1 + c2 + c3 + c4); */
+
+       /*  boolean dogru = true;
+        boolean yanlis = false;
+        System.out.println(yanlis); */
+
+        /* int a = 7;
+        int b = 3;
+        boolean sonuc = (a==b); //Tek eşittir atama operatörüdür bu nedenle çift eşittir kullanılır.//
+        System.out.println(sonuc);
+
+        int c = 7;
+        int d = 3;
+        boolean sonuc1 = (c !=d);
+        System.out.println(sonuc1);
+
+        int e = 3;
+        int f = 3;
+        boolean sonuc2 = (e==f);
+        System.out.println(sonuc2); */
+
+        /* byte b = 0;
+        int a = b++;
+        System.out.print(a); */
+
+        /* int a = 10;
+        int b = 3;
+        int carpma = a*b;
+        float  bolme = a/b;
+        int toplama = a+b;
+        int cikarma = a-b;
+        int modAlma = a%b;
+        boolean yanlis = a==b;
+        boolean dogru = a!=b;
+        boolean buyuk = a>b;
+        boolean kucuk = a<b;
+        boolean ve = (a>=b) && (a<=b);
+        boolean veya = (a>=b) || (a<=b);
+        boolean veyaDegili = !((a>=b)||(a<=b));
+        int kosul = (a==1)? 1:0;
+
+
+        System.out.println(carpma);
+        System.out.println(bolme);
+        System.out.println(toplama);
+        System.out.println(cikarma);
+        System.out.println(modAlma);
+        System.out.println(yanlis);
+        System.out.println(dogru);
+        System.out.println(buyuk);
+        System.out.println(kucuk);
+        System.out.println(ve);
+        System.out.println(veya);
+        System.out.println(veyaDegili);
+        System.out.println(kosul); */
+
+        /* int m = -12;
+        int n = -5;
+        int sonuc = m%n;
+        System.out.println(sonuc); */
+
+
+
+
+    }
+}
